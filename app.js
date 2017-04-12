567a068c88de76f4b73ada390336bbb88afbfd30
Airyque Ervin <airyque@gmail.com>
2017-04-12 10:38:44 -0700

    Rendering stuff on page

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // TODO
+
+var TodoList = (props) => (
+  <ul>
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
+    <li>{props.todos[2]}</li>
+  </ul>
+);
+
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
