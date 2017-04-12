28c452f79c0ff736fe86cf1d45019721632509cc
Airyque Ervin <airyque@gmail.com>
2017-04-12 10:56:50 -0700

    created a grocery list and rendered elements

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 // TODO
 
+var GroceryList = () => (
+  <div>
+    <h2>Get Food!</h2>
+    <Pizza toppings={['Pepperoni', 'Cheese', 'Sauce']}/>
+    <Bagles spread={['Plain Cream Cheese', 'Chive and Onion', 'Strawberry']} />
+  </div>
+);
+
+var Pizza = (props) => (
+  <div>
+    <h3>Pizza is EVERYTHING!</h3>
+    <ul>
+      <li>{props.toppings[0]}</li>
+      <li>{props.toppings[1]}</li>
+      <li>{props.toppings[2]}</li>
+    </ul>
+  </div>
+); 
+var Bagles = (props) => (
+  <div>
+    <h3>Bagels Fridays!</h3>
+    <ul>
+      <li>{props.spread[0]}</li>
+      <li>{props.spread[1]}</li>
+      <li>{props.spread[2]}</li>
+    </ul>
+  </div>
+); 
+
+ReactDOM.render(<GroceryList />, document.getElementById('app')); 
+
 var TodoList = (props) => (
   <ul>
     <li>{props.todos[0]}</li>
@@ -16,4 +47,4 @@ var App = () => (
   </div>
 );
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<App />, document.getElementById('app'));
